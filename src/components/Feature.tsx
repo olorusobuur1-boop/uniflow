b8ac0389e0d5291147aee8ce802adb0017de82c2
olorusobuur1-boop <olorusobuur1@gmail.com>
2026-09-07 23:49:59 +0100

    done for the night

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,16 +1,43 @@
-// import React from 'react'
+import React from 'react'
+import dashboard from "../assets/images/dashboard.png"
+import cgpa from "../assets/images/cgpa.png"
+import timetable from "../assets/images/timetable.png"
 
 const Feature = () => {
   return (
     <>
       <section>
-        <h3>Features</h3>
+        <h3 className='ft'>Features</h3>
         <div className="wrapper">
         <div className="feature-container">
-            
+            <img src={dashboard} alt="" />
+            <h4>Unified Dashboard</h4>
+            <p>Analysis, Records, Data are 
+              collected and stored in our end. And 
+              they can be viewed on your
+               dashboard...
+            </p>
+            <a href=''>Learn more &#8594;</a>
         </div>
-        <div className="feature-container"></div>
-        <div className="feature-container"></div>
+        <div className="feature-container">
+          <img src={cgpa} alt="" />
+            <h4>CGPA & Course Tracking</h4>
+            <p>Calculates student CGPA and
+               track their courses
+              as well as improvements made so far...
+            </p>
+            <a href=''>Learn more &#8594;</a>
+        </div>
+        <div className="feature-container">
+          <img src={timetable} alt="" />
+            <h4>Smart Timetable & Assignments</h4>
+            <p>
+              Smart timetables are created to 
+              improve the output of the student in all
+              academic spheres... 
+            </p>
+            <a href=''>Learn more &#8594;</a>
+        </div>
         </div>
       </section>
     </>
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,14 @@
 // import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Feature from '../components/Feature'
 
 const Homepage = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <Feature/>
     </>
   )
 }
